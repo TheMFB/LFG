93265b81eecd5e20c930721fb543bcacc3d87afc
Michael Burgess <mike@waev.com>
2023-03-16 07:17:55 -0400

    Updated Background Testimonials

--- src/Components/Testimonials.js
+++ src/Components/Testimonials.js
@@ -4,36 +4,41 @@ class Testimonials extends Component {
   render() {
     if (!this.props.testimonials) return null;
 
-    const testimonials = this.props.testimonials.testimonials.map(function (testimonials) {
+    const testimonials = this.props.testimonials.testimonials.map(function (testimonials, i) {
       return (
-        <li key={testimonials.user}>
-          <blockquote>
-            <p>{testimonials.text}</p>
-            <cite>{testimonials.user}</cite>
-          </blockquote>
-        </li>
+
+        <section id={`testimonials${i}`}>
+          <div className="text-container">
+            <div className="row">
+              <div className="two columns header-col">
+                {/* {this.props.title && (
+
+                  <h1>
+                    <span>{this.props.testimonials.title}</span>
+                  </h1>
+                )} */}
+              </div>
+
+              <div className="ten columns flex-container">
+                <ul className="slides">
+                  <li key={testimonials.user}>
+                    <blockquote>
+                      <p>{testimonials.text}</p>
+                      <cite>{testimonials.user}</cite>
+                    </blockquote>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </section>
       );
     });
 
     return (
-      <section id="testimonials">
-        <div className="text-container">
-          <div className="row">
-            <div className="two columns header-col">
-              {this.props.title && (
-
-                <h1>
-                  <span>{this.props.testimonials.title}</span>
-                </h1>
-              )}
-            </div>
-
-            <div className="ten columns flex-container">
-              <ul className="slides">{testimonials}</ul>
-            </div>
-          </div>
-        </div>
-      </section>
+      <>
+        {testimonials}
+      </>
     );
   }
 }
